d1bd7a028c8d7cd554afcc5058914816c643dd35
raa0303 <raa0303@gmail.com>
2023-10-24 22:36:05 +0900

    3장 파이보 서비스 개발!
    03-2 게시판 페이징 기능 추가하기

--- project/myproject/pybo/views/question_views.py
+++ project/myproject/pybo/views/question_views.py
@@ -2,14 +2,16 @@ from datetime import datetime
 from flask import Blueprint, render_template, request, url_for
 from werkzeug.utils import redirect
 from .. import db
-from pybo.models import Question
+from ..models import Question
 from ..forms import QuestionForm, AnswerForm
 
 bp = Blueprint('question', __name__, url_prefix='/question')
 
 @bp.route('/list/')
 def _list():
+    page = request.args.get('page', type=int, default=1) # 페이지
     question_list = Question.query.order_by(Question.create_date.desc())
+    question_list = question_list.paginate(page, per_page=10)
     return render_template('question/question_list.html', question_list=question_list)
 
 @bp.route('/detail/<int:question_id>/')
