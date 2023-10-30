40e0168eab9c8cfcd2324a6fb1d6911ddaf1688a
raa0303 <raa0303@gmail.com>
2023-10-30 22:13:20 +0900

    3장 파이보 서비스 개발!
    03-8 모델 수정하여 파이보 기능 다듬기

--- project/myproject/pybo/views/auth_views.py
+++ project/myproject/pybo/views/auth_views.py
@@ -1,10 +1,20 @@
+import functools
 from flask import Blueprint, url_for, render_template, flash, request, session, g
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import redirect
 
+
 from pybo import db
 from pybo.forms import UserCreateForm, UserLoginForm
 from pybo.models import User
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        return view(**kwargs)
+    return wrapped_view
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
--- project/myproject/pybo/views/question_views.py
+++ project/myproject/pybo/views/question_views.py
@@ -1,6 +1,7 @@
 from datetime import datetime
-from flask import Blueprint, render_template, request, url_for
+from flask import Blueprint, render_template, request, url_for, g
 from werkzeug.utils import redirect
+from pybo.views.auth_views import login_required
 from .. import db
 from ..models import Question
 from ..forms import QuestionForm, AnswerForm
@@ -21,10 +22,11 @@ def detail(question_id):
     return render_template('question/question_detail.html', question=question, form=form)
 
 @bp.route('/create/', methods=('GET', 'POST'))
+@login_required
 def create():
     form = QuestionForm()
     if request.method == 'POST' and form.validate_on_submit():
-        question = Question(subject=form.subject.data, content=form.content.data, create_date=datetime.now())
+        question = Question(subject=form.subject.data, content=form.content.data, create_date=datetime.now(), user=g.user)
         db.session.add(question)
         db.session.commit()
         return redirect(url_for('main.index'))
