6ac42910b05500cf3ee97388d9727ffcd618324e
raa0303 <raa0303@gmail.com>
2023-11-14 21:23:42 +0900

    4. 세상에 선보이는 파이보 서비스!
    04-6 서버,개발 환경을 위한 config 분리하기

--- project/myproject/pybo/__init__.py
+++ project/myproject/pybo/__init__.py
@@ -3,7 +3,6 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
-import config
 
 naming_convention = {
     "ix": "ix_%(column_0_label)s",
@@ -17,7 +16,7 @@ migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(config)
+    app.config.from_envvar('APP_CONFIG_FILE')
 
     # ORM
     db.init_app(app)
