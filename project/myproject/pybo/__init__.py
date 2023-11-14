00aa4cb6ac7c704d82278171954edb276cad68fc
raa0303 <raa0303@gmail.com>
2023-11-14 23:00:27 +0900

    4. 세상에 선보이는 파이보 서비스!
    04-12 서비스답게 오류 페이지 다듬기

--- project/myproject/pybo/__init__.py
+++ project/myproject/pybo/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
@@ -14,9 +14,13 @@ naming_convention = {
 db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
 migrate = Migrate()
 
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 def create_app():
     app = Flask(__name__)
     app.config.from_envvar('APP_CONFIG_FILE')
+    app.register_error_handler(404, page_not_found)
 
     # ORM
     db.init_app(app)
